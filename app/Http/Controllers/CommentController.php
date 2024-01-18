7f33c805fbbefa8d2c725c5d462e0e345c31ad89
jangrus <jan.gruszka8@gmail.com>
2024-01-18 22:13:32 +0100

    add controlers for posts and comments

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class CommentController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Post $post)
+    {
+        $data = $request->validate(['content' => ['required', 'string', 'max:255']]);
+
+        $post->comments()->create([...$data, 'user_id' => $request->user()->id]);
+
+        return to_route('posts.show', $post)->withFragment('comments');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post, Comment $comment)
+    {
+        if (!Gate::allows('delete', $comment)) {
+            return abort(403);
+        }
+
+        $comment->delete();
+
+        return to_route('posts.show', $post)->withFragment('comments');
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->with('user')->paginate(10),
+        ]);
+    }
+
+    public function create()
+    {
+        $category = Category::all();
+        return view('posts.create', compact('category'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'header' => 'required',
+            'content' => 'required',
+            'topic_id' => 'required|exists:category,id',
+        ]);
+
+        $postData = $request->all();
+        $postData['created_by'] = auth()->id();
+
+        Post::create($postData);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post,
+            'comments' => $post->comments()->latest()->with('user')->paginate(10),
+
+
+        ]);
+    }
+}
