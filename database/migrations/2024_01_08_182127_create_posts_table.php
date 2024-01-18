59539d1e09f67fb5a3ae4edcd84edd11eaec51c9
jangrus <jan.gruszka8@gmail.com>
2024-01-18 22:10:09 +0100

    database migration changes

--- database/migrations/2024_01_08_182127_create_posts_table.php
+++ database/migrations/2024_01_08_182127_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,10 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('header');
             $table->longText('content');
+            $table->foreignIdFor(User::class);
             $table->unsignedBigInteger('topic_id');
             $table->foreign('topic_id')->references('id')->on('category')->onDelete('cascade');
             $table->unsignedBigInteger('created_by')->nullable();
--- database/migrations/2024_01_08_183020_create_comments_table.php
+++ database/migrations/2024_01_08_183020_create_comments_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,12 +14,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('comment', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Post::class)->nullable();
+            $table->foreignIdFor(Comment::class)->nullable();
             $table->unsignedBigInteger('parent_post')->nullable();;
-            $table->foreign('parent_post')->references('id')->on('post')->onDelete('cascade');
+            $table->foreign('parent_post')->references('id')->on('posts')->onDelete('cascade');
             $table->unsignedBigInteger('parent_comment')->nullable();
-            $table->foreign('parent_comment')->references('id')->on('comment')->onDelete('cascade');
+            $table->foreign('parent_comment')->references('id')->on('comments')->onDelete('cascade');
             $table->string('header');
             $table->text('content');
             $table->unsignedBigInteger('created_by')->nullable();
